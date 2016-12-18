dc5a81de87a30fa84876d5dd8980ca5b12af8e55
Zhang LiangZhi <3717591@qq.com>
2016-12-18 23:40:10 +0800

    go

--- SavePassword/SavePassword/ViewController.swift
+++ SavePassword/SavePassword/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var aa:String = NSLocalizedString("teststr", comment: "")
+        print(aa)
     }
 
     override func didReceiveMemoryWarning() {
