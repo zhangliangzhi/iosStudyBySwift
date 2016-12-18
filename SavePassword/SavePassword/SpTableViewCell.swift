0dc1509c0a140685a297c66cd980111fdb60b055
Zhang LiangZhi <3717591@qq.com>
2016-12-19 00:51:34 +0800

    go

---
+++ SavePassword/SavePassword/SpTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpTableViewCell.swift
+//  SavePassword
+//
+//  Created by ZhangLiangZhi on 2016/12/19.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class SpTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
