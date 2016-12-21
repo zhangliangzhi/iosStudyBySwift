cdab03ad362dfbaaa9d9d81c9e0d04b6968a560f
Zhang LiangZhi <3717591@qq.com>
2016-12-22 01:30:01 +0800

    go

--- SavePassword/SavePassword/AddUpViewController.swift
+++ SavePassword/SavePassword/AddUpViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class AddUpViewController: UIViewController {
+class AddUpViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
 
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sortID: UITextField!
     @IBOutlet weak var tfTitle: UITextField!
     @IBOutlet weak var urltxt: UITextField!
-    @IBOutlet weak var sortID: UITextField!
+    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        sortID.delegate = self
+        tfTitle.delegate = self
+        urltxt.delegate = self
+        textView.delegate = self
+        tfTitle.becomeFirstResponder()
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == sortID {
+            tfTitle.becomeFirstResponder()
+        }else if textField == tfTitle {
+            urltxt.becomeFirstResponder()
+        }else if textField == urltxt {
+            textView.becomeFirstResponder()
+        }else{
+            
+        }
+        return true
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == "\n" {
+            textView.resignFirstResponder()
+            
+        }
+        
+        return true
     }
     
     @IBAction func saveAction(_ sender: Any) {
