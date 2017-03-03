7fa2794515127346aad072230e26b237ec43dfbe
Zhang LiangZhi <3717591@qq.com>
2017-03-03 12:02:51 +0800

    o

---
+++ RandomNum/RandomNum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomNum
+//
+//  Created by ZhangLiangZhi on 2016/12/31.
+//  Copyright © 2016年 xigk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
