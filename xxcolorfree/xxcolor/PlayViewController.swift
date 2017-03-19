3829518813a4d2ca1b68365f7b77eb9fc573b285
Zhang LiangZhi <3717591@qq.com>
2017-03-20 02:37:25 +0800

    o

--- xxcolorfree/xxcolor/PlayViewController.swift
+++ xxcolorfree/xxcolor/PlayViewController.swift
@@ -15,8 +15,7 @@ class PlayViewController: UIViewController {
     var _ss = 0
     var a = 0
     var p = 2
-    var timer:Timer!
-    var iSec = 60
+
     var totalScore = 0
     
 
@@ -27,14 +26,7 @@ class PlayViewController: UIViewController {
         
     }
     
-    func timeEverySec() {
-        iSec -= 1
-        self.title =  NSLocalizedString("Remaining Time", comment: "") + ":" + "\(iSec)"
-        
-        if iSec <= 0 {
-            GameOver()
-        }
-    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         MobClick.event("UMPLAY")
@@ -46,10 +38,9 @@ class PlayViewController: UIViewController {
         totalScore = 0
         gScore = 0
         
-        scoreLabel.text = "\(0)" + " " + NSLocalizedString("point", comment: "")
-        self.title = NSLocalizedString("Remaining Time", comment: "") + ":60"
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timeEverySec), userInfo: nil, repeats: true)
-        iSec = 60
+        self.title = "\(0)" + " " + NSLocalizedString("point", comment: "")
+        
+
         
         play1()
     }
@@ -61,22 +52,21 @@ class PlayViewController: UIViewController {
             rv?.removeFromSuperview()
         }
         
-        scoreLabel.text = "\(self.a)" + " " + NSLocalizedString("point", comment: "")
+        self.title = "\(self.a)" + " " + NSLocalizedString("point", comment: "")
         totalScore = self.a
         
         self.a += 1;
         if (self.a % 5 == 0) {
             self.p += 1;
         }
-        let height = self.view.bounds.size.height / CGFloat(p)
-        let width = self.view.bounds.size.width / CGFloat(p)
-        
 
-        
-        
-        let v:UIView = UIView(frame: CGRect(x: 0, y: 64, width: self.view.bounds.size.width, height: height*CGFloat(p)-100))
-        v.tag = 888
+        let v = UIView(frame: CGRect.zero)
         self.view.addSubview(v)
+        v.snp.makeConstraints { (make) in
+            make.edges.equalTo(UIEdgeInsets(top: (navigationController?.navigationBar.frame.height)!, left: 0, bottom: 0, right: 0))
+        }
+        v.backgroundColor = UIColor.gray
+        v.tag = 888
         
         self.view.backgroundColor = UIColor.white
         v.backgroundColor = UIColor.white
@@ -86,29 +76,60 @@ class PlayViewController: UIViewController {
         let hue:CGFloat = ( CGFloat(arc4random() % 128) / 256.0 )+0.5;
         let saturation:CGFloat = ( CGFloat(arc4random() % 128) / 256.0 ) + 0.5;
         let brightness:CGFloat = ( CGFloat(arc4random() % 128) / 256.0 ) + 0.5;
+
         
-        for i in 0..<p*p {
-            let imageButton = UIButton(frame: CGRect(x: width*(CGFloat(i/p)+0.025), y: ((height*0.83) * CGFloat(i%p)), width: width*0.95, height: height*0.8))
-            
-            imageButton.autoresizingMask = [UIViewAutoresizing.flexibleTopMargin , UIViewAutoresizing.flexibleBottomMargin,.flexibleLeftMargin , .flexibleRightMargin]
-            
-            if (i == Int(s) ) {
-                let a2 = CGFloat(a) * 0.01
-                let a3:CGFloat = 0.5 + a2
-                imageButton.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: a3)
-                imageButton.addTarget(self, action: #selector(play1), for: .touchDown)
-            }else{
-                imageButton.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
-                imageButton.addTarget(self, action: #selector(GameOver), for: .touchDown)
+        var arrBtns = [UIButton]()
+        for i in 0..<p{
+            for j in 0..<p {
+                let indexOfKeys = i*p + j
+                let btn = UIButton(type: .custom)
+                v.addSubview(btn)
+                arrBtns.append(btn)
+                
+                btn.backgroundColor = UIColor.red
+                
+                btn.layer.borderWidth = 5.0*(2.0/CGFloat(p)) + 0.5
+                btn.layer.borderColor = UIColor.white.cgColor
+                
+                
+                btn.snp.makeConstraints({ (make) in
+                    make.width.equalTo(v.snp.width).multipliedBy(1.0/Double(p))
+                    make.height.equalTo(v.snp.height).multipliedBy(1.0/Double(p))
+                    
+                    //添加垂直位置约束
+                    if i == 0{
+                        make.top.equalTo(0)
+                    }else{
+                        make.top.equalTo(arrBtns[indexOfKeys-p].snp.bottom)
+                    }
+                    
+                    //添加水平位置约束
+                    if j == 0{
+                        make.left.equalTo(0)
+                    }else{
+                        make.left.equalTo(arrBtns[indexOfKeys-1].snp.right)
+                    }
+                    
+                    
+                })
+                
+                // 添加事件
+                if (indexOfKeys == Int(s) ) {
+                    let a2 = CGFloat(a) * 0.01
+                    let a3:CGFloat = 0.5 + a2
+                    btn.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: a3)
+                    btn.addTarget(self, action: #selector(play1), for: .touchDown)
+                }else{
+                    btn.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+                    btn.addTarget(self, action: #selector(GameOver), for: .touchDown)
+                }
             }
-            v.addSubview(imageButton)
         }
  
     }
     
     func GameOver() -> Void {
-        timer.invalidate()
-        timer = nil
+
         self.title = ""
         
         gScore = self.totalScore
