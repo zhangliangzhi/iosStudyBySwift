6c1e5e95f385da807df3e397948bbd834b3f6a5d
Zhang LiangZhi <3717591@qq.com>
2017-04-04 19:22:53 +0800

    o

--- xxcolorfree/xxcolor/ViewController.swift
+++ xxcolorfree/xxcolor/ViewController.swift
@@ -17,6 +17,8 @@ let context = appDelegate.persistentContainer.viewContext
 var arrGlobalSet:[CurGlobalSet] = []
 var gGlobalSet:CurGlobalSet?
 
+var arrGuanka:[Guanka] = []
+
 var gScore = 0
 
 class ViewController: UIViewController, GKGameCenterControllerDelegate {
@@ -24,13 +26,16 @@ class ViewController: UIViewController, GKGameCenterControllerDelegate {
     var mainv:UIView!
     var playv:UIView!
 
+    var sv:UIScrollView!
+    let maxLevel = 52
     @IBOutlet weak var lblCoin: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        autoPlayer()
+        //test
+//        autoPlayer()
         self.title = NSLocalizedString("Find Color", comment: "")
 //        self.view.backgroundColor = UIColor.darkGray
         
@@ -85,8 +90,20 @@ class ViewController: UIViewController, GKGameCenterControllerDelegate {
             make.centerX.equalTo(btnShowLocal)
             make.top.equalTo(btnShowLocal.snp.bottom).offset(30)
         }
+        
+        addScrollView()
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // 检测设备方向
+         NotificationCenter.default.addObserver(self, selector: #selector(receivedRotation), name: .UIDeviceOrientationDidChange, object: nil)
+    }
+
+    
+
     func add100Level() {
         
     }
@@ -147,6 +164,13 @@ class ViewController: UIViewController, GKGameCenterControllerDelegate {
             print("Setting coreData error")
         }
         
+        arrGuanka = []
+        do {
+            arrGuanka = try context.fetch(Guanka.fetchRequest())
+        }catch {
+            print("get guanka error")
+        }
+        
         if arrGlobalSet.count > 0 {
             gGlobalSet = arrGlobalSet[0]
             
@@ -179,6 +203,105 @@ class ViewController: UIViewController, GKGameCenterControllerDelegate {
         
         getCoreData()
     }
+    
+    // 把66级关卡加在一个scrollview里面
+    func addScrollView() {
+        sv = UIScrollView()
+        self.view.addSubview(sv)
+        sv.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.bottom.equalTo(self.view)
+            make.top.equalTo(lblCoin.snp.bottom).offset(8)
+            make.centerX.equalTo(self.view)
+        }
+        sv.backgroundColor = UIColor.gray
+        
+        sv.contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height*3.25)
+        
+        // 68个button
+        var arrBtns = [UIButton]()
+        let ikuan = 4
+        let igao = 13
+        for i in 0..<igao{
+            for j in 0..<ikuan {
+                let indexOfKeys = i*ikuan + j
+                let btn = UIButton(type: .custom)
+                sv.addSubview(btn)
+                arrBtns.append(btn)
+                
+                btn.layer.borderWidth = 2
+                btn.layer.borderColor = UIColor.white.cgColor
+                
+                btn.setTitleColor(UIColor.white, for: UIControlState.normal)
+                btn.backgroundColor = UIColor(red: 102/255, green: 184/255, blue: 77/255, alpha: 1)
+                
+                btn.snp.makeConstraints({ (make) in
+                    make.width.equalTo(self.view.snp.width).multipliedBy(1.0/Double(ikuan))
+                    make.height.equalTo(self.view.snp.height).multipliedBy(1.0/Double(ikuan))
+                    
+                    //添加垂直位置约束
+                    if i == 0{
+                        make.top.equalTo(0)
+                    }else{
+                        make.top.equalTo(arrBtns[indexOfKeys-ikuan].snp.bottom)
+                    }
+                    print(j)
+                    //添加水平位置约束
+                    if j == 0{
+                        make.left.equalTo(0)
+                    }else{
+                        make.left.equalTo(arrBtns[indexOfKeys-1].snp.right)
+                    }
+                })
+/*
+                // 添加事件
+                if (indexOfKeys == Int(s) ) {
+                    let a2 = CGFloat(a) * 0.01
+                    let a3:CGFloat = 0.5 + a2
+                    btn.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: a3)
+                    btn.addTarget(self, action: #selector(goLevelPlay), for: .touchDown)
+                }else{
+                    btn.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+                    btn.addTarget(self, action: #selector(goLevelPlay), for: .touchDown)
+                }
+ */
+                
+                btn.setTitle("\(indexOfKeys+1)", for: .normal)
+                
+            }
+        }
+        
+        
+    }
+    
+    //
+    func goLevelPlay() {
+        print("play go level")
+    }
+    
+    //通知监听触发的方法
+    func receivedRotation(){
+        sv.contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height*3.25)
+        
+        // 屏幕方向
+//        switch UIDevice.current.orientation {
+//        case UIDeviceOrientation.unknown:
+//            print("方向未知")
+//        case .portrait: // Device oriented vertically, home button on the bottom
+//            print("屏幕直立")
+//        case .portraitUpsideDown: // Device oriented vertically, home button on the top
+//            print("屏幕倒立")
+//        case .landscapeLeft: // Device oriented horizontally, home button on the right
+//            print("屏幕左在上方")
+//        case .landscapeRight: // Device oriented horizontally, home button on the left
+//            print("屏幕右在上方")
+//        case .faceUp: // Device oriented flat, face up
+//            print("屏幕朝上")
+//        case .faceDown: // Device oriented flat, face down
+//            print("屏幕朝下")
+//        }
+    }
+    
     
 }
 
