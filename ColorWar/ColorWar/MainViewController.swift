33f012d67a4f6760d2a620e4101cc1e6c4e2b84f
Zhang LiangZhi <3717591@qq.com>
2017-06-06 04:51:25 +0800

    o

--- ColorWar/ColorWar/MainViewController.swift
+++ ColorWar/ColorWar/MainViewController.swift
@@ -7,29 +7,24 @@
 //
 
 import UIKit
+import CoreData
+
+
+let appDelegate = UIApplication.shared.delegate as! AppDelegate
+let context = appDelegate.persistentContainer.viewContext
+var arrGlobalData:[GlobalData] = []
+var curGlobal:GlobalData?
+
 
 class MainViewController: UIViewController {
 
+    var rootv:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
