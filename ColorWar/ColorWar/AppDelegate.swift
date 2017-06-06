06a69430d015fe246c7647f7425671426f598c16
Zhang LiangZhi <3717591@qq.com>
2017-06-06 19:48:47 +0800

    o

--- ColorWar/ColorWar/AppDelegate.swift
+++ ColorWar/ColorWar/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        self.window?.rootViewController = MainViewController()
+        self.window?.rootViewController = RTabBarViewController()
         return true
     }
 
--- ColorWar/ColorWar/RTabBarViewController.swift
+++ ColorWar/ColorWar/RTabBarViewController.swift
@@ -13,23 +13,33 @@ class RTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        creatSubViewControllers()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func creatSubViewControllers(){
+        let v1  = MainViewController()
+        let item1 : UITabBarItem = UITabBarItem (title: "象形字典", image: UIImage(named: "honor"), selectedImage: UIImage(named: "honor"))
+        v1.tabBarItem = item1
+        
+        let v2 = MainViewController()
+        let item2 : UITabBarItem = UITabBarItem (title: "荣誉", image: UIImage(named: "icon_Battle"), selectedImage: UIImage(named: "icon_Battle"))
+        v2.tabBarItem = item2
+        
+        
+        let v3 = MainViewController()
+        let item3 : UITabBarItem = UITabBarItem (title: "字", image: UIImage(named: "icon_Battle"), selectedImage: UIImage(named: "icon_Battle"))
+        v3.tabBarItem = item3
+        
+        
+        
+        let n1 = UINavigationController(rootViewController: v1)
+        let n2 = UINavigationController(rootViewController: v2)
+        let n3 = UINavigationController(rootViewController: v3)
+        let tabArray = [n1, n2, n3]
+        self.viewControllers = tabArray
+        
+        //默认选中的是游戏主界面视图
+        self.selectedIndex = 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
