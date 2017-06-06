38327eccfd26f2d2cf9934bfb8395907f70cea12
Zhang LiangZhi <3717591@qq.com>
2017-06-06 11:11:14 +0800

    o

--- ColorWar/ColorWar/MainViewController.swift
+++ ColorWar/ColorWar/MainViewController.swift
@@ -24,8 +24,66 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        rootv = UIView()
+        self.view.addSubview(rootv)
+        rootv.snp.makeConstraints { (make) in
+            make.width.equalTo(self.view)
+            make.centerX.equalTo(self.view)
+            make.bottom.equalTo(self.view).offset(-44)
+            make.top.equalTo(self.view).offset(64)
+        }
+        
+        initUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.getCoreData()
+        firstOpenAPP()
+        
         
     }
 
+    func initUI() {
+        
+    }
+    
+    // 第一次打开app，加入测试数据
+    func firstOpenAPP() -> Void {
+        // 初始化
+        if arrGlobalData.count > 0 {
+            return
+        }
+        
+        let oneGlobalSet = NSEntityDescription.insertNewObject(forEntityName: "GlobalData", into: context) as! GlobalData
+        
+        oneGlobalSet.coin = 0
+        oneGlobalSet.diamond = 0
+        oneGlobalSet.exp = 0
+        oneGlobalSet.id = 0
+        oneGlobalSet.token = ""
+        oneGlobalSet.level = 0
+        oneGlobalSet.nickname = ""
+        
+        context.insert(oneGlobalSet)
+        appDelegate.saveContext()
+        self.getCoreData()
+    }
+    
+    func getCoreData() -> Void {
+        arrGlobalData = []
+        
+        do {
+            arrGlobalData = try context.fetch(GlobalData.fetchRequest())
+        }catch {
+            print("Setting coreData error")
+        }
+        
+        
+        // last
+        if arrGlobalData.count > 0 {
+            curGlobal = arrGlobalData[0]
+            
+        }
+    }
 
 }
